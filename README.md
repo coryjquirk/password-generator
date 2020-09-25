136db7d1905eb50ca7895f07eb5847b4e2fbc6d2
Cory Quirk <coryjquirk@gmail.com>
2020-09-25 02:01:36 -0500

    functional application

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# password-generator
+# Password Generator
+
+## Description
+
+This is an application designed to produce a random password consisting of symbols, numbers, and letters. The result is a secure password that is difficult for humans and computers to guess.
+
+## URL of application: 
+
+* https://coryjquirk.github.io/password-generator/
+
+* screenshot of the deployed application: https://coryjquirk.github.io/password-generator/images/screenshot.png
+
+
+## How to Use
+
+Hit the red Generate Password button to start the program.
+
+The program will ask you how many characters you would like in your password (minimum 8 characters, maximum 128 characters). Then it will ask you if you want Numbers, Symbols, Upper Case letters, and Lower Case letters. The result will be a string of characters of your desired length.
+
+Copy the generated password to use.
+
+Refresh the page before generating a new password.
+
+If you fail to select at least one of the character categories, you will be prompted to start over again. Additionally, if you enter no character length or a character length outside of 8-128, you will be asked to try again. Hit the Generate Password button to begin anew. 
+
+## Installation
+
+N/A
+
+## Credits
+
+Thank you to my classmates, instructors, and TAs in their guidance with writing the Javascript for this project, and to Owen Höglund for assistance through the process of developing the code for the password generation.
---
+++ script.js
@@ -0,0 +1,117 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = passwordArray;
+}
+
+// Add event listener to generate button. 
+  //generateBtn means doing something to the button that's in the html file
+generateBtn.addEventListener("click", writePassword);
+
+//collect all answers in an object and return
+var password = '';
+
+//character string arrays represented as list of vars
+var Symbols = '~!@#$%&-+=_';
+var SymbolsLength = 11;
+var Numbers = '0123456789';
+var NumbersLength = 10
+var UpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var UpperCaseLength = 26;
+var LowerCase = 'abcdefghijklmnopqrstuvwxyz';
+var LowerCaseLength = 26;
+var passwordArray = '';
+
+//variable declarations
+var confirmLength;
+var confirmSymbols;
+var confirmNumbers;
+var confirmUpperCase;
+var confirmLowerCase;
+
+//create generate password function
+  //if special characters, push random characters to a new array
+  //if numeric push from numeric array to new array
+
+  //make sure user picks at least 1 of the conditions they want to include...
+  //...in their new password (e.g. symbols, #s, uppercase, lowercase).
+
+function generatePassword() {
+  //set password length/complexity
+  var Length = parseInt(prompt("Enter password length (8 to 128 characters)"));
+  if(Length=null){
+    window.alert("Please enter your desired password length.");
+  }
+  while (Length > 128 || Length < 8) {
+      Length = parseInt(prompt ("Please enter a password length (min 8, max 128)"));
+    }
+  var confirmSymbols = confirm("Include special characters?");
+  var confirmNumbers = confirm("Include numbers?");
+  var confirmUpperCase = confirm("Include upper case letters?");
+  var confirmLowerCase = confirm("Include lower case letters?");
+  if(confirmSymbols===false&&confirmNumbers===false&&confirmUpperCase===false&&confirmLowerCase===false){
+    window.alert("Please select at least one category.");
+  }
+
+  while (passwordArray.length < Length) {
+    if(confirmSymbols) {
+      passwordArray = passwordArray.concat(Symbols.charAt(Math.floor(Math.random() * SymbolsLength)))
+      if(passwordArray.length===Length){
+        break;
+      }
+    }
+    if (confirmNumbers) {
+      passwordArray = passwordArray.concat(Numbers.charAt(Math.floor(Math.random() * NumbersLength)))
+      if(passwordArray.length===Length){
+        break;
+      }
+    }
+    if (confirmUpperCase) {
+      passwordArray = passwordArray.concat(UpperCase.charAt(Math.floor(Math.random() * UpperCaseLength)))
+      if(passwordArray.length===Length){
+        break;
+      }
+    }
+    if(confirmLowerCase) {
+      passwordArray = passwordArray.concat(LowerCase.charAt(Math.floor(Math.random() * LowerCaseLength)))
+      if(passwordArray.length===Length){
+        break;
+      }
+    }
+  }
+
+
+  for(var i = 0; i < 100; i++) {
+
+  }
+
+
+  return passwordArray;
+} 
+
+
+
+//for loop that will push through all possible characters according to length selected
+  //push final result of password into another array and then print
+
+
+  // Var password = '';
+  // Var length;
+  // Length = (user input shit);
+  // [For loop which runs x times]{
+  // Var rand = random variable;
+  // Var type = rand modulo Y (y is the number of lists, like symbols, letters, uppercase)
+  // If type = (1,2,3...)
+  //     Then 
+  //            Add a character of type 1,2,3...
+  // )
+  // 
+  // Then the adding the character thing would be however you add a character to the blank string you made in the beginning
+  // 
+  // Using the charAt function we discussed
+
